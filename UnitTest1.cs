f2c52cf75a7372f32dde779e8d2ccaf8a78c0b1c
Charles Mize <52986137+chvrl@users.noreply.github.com>
2020-03-02 13:20:54 -0800

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.ApplicationInsights;
+using Xunit;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            TelemetryClient telemetryClient = null;
+        }
+    }
+}
